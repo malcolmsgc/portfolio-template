cb72d56643f514e7a56c244ad1749148f7a04c2e
Malcolm Cumming <bemjogar@gmail.com>
2017-11-20 14:41:07 -0600

    Variable skill bar length based on state

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-const Skill = (props) => (
+const Skill = (props) => {
+  const barLengthStyle = {
+    width : props.proficiency + "%"
+  } 
+  return (
   <div className="skill">
     <h3>{props.skill}</h3>
     <div className="skillbar">
-      <div className="proficiency"></div>
+      <div className="proficiency" style={barLengthStyle}></div>
     </div>
   </div>
 )
+};
 
 
 
